1c0382b3da4e4c7a7ea634f7140fd23d8b547a38
hkfrohnow <heiner.kuhlmann@web.de>
2026-09-13 07:26:16 +0200

    Update Supabase client API key
    Updated the Supabase client initialization with a new API key.

--- src/lib/supabase-client.ts
+++ src/lib/supabase-client.ts
@@ -6,7 +6,7 @@ export function createClient() {
   if (!client) {
     client = createBrowserClient(
       "https://kcvpmllxroxhnrhyhxfp.supabase.co",
-      "eyJhbGci••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••"
+      "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImtjdnBtbGx4cm94aG5yaHloeGZwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODkyMzc2MTYsImV4cCI6MjEwNDgxMzYxNn0.imBL795spt4-Xxf35ugtNOo1wwkAclRsQ9CY4L9AAaI"
     );
   }
   return client;
